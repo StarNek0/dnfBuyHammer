9359e96d1549e87a998bd0eb339959f5b1a2d722
zsdostar <zsdzzydos@gmail.com>
2019-09-17 11:38:45 +0800

    代码重构

--- main.go
+++ main.go
@@ -14,7 +14,7 @@ func ReadMouseLeftClickPoint() Point {
 	// 录制单次鼠标点击事件
 	defer time.Sleep(time.Millisecond * 300)
 	p := Point{}
-	if ok := robotgo.AddEvent("mleft"); ok {
+	if robotgo.AddEvent("mleft") {
 		p.x, p.y = robotgo.GetMousePos()
 		fmt.Printf("---你按下左键, 坐标为(%d, %d)---\n", p.x, p.y)
 	}
@@ -22,31 +22,24 @@ func ReadMouseLeftClickPoint() Point {
 }
 func WriteMouseLeftClickList(q *bool, mousePointList *[]Point) {
 	// 重放鼠标点击事件队列
-	var i int
-	for *q == false {
+	for i := 1; !*q; i++ {
 		for _, p := range *mousePointList {
 			robotgo.MovesClick(p.x, p.y, "left", false)
 			time.Sleep(time.Millisecond * 500)
 		}
-
-		i++
 		fmt.Println("执行", i, "次")
 	}
 }
 func main() {
-
 	startMsg := robotgo.ShowAlert("提示", "点击确定开始录制3个按键", "确定", "取消")
 	if startMsg == 0 { //确定0，取消1
-
 		mousePointList := []Point{ReadMouseLeftClickPoint(), ReadMouseLeftClickPoint(), ReadMouseLeftClickPoint()}
 
 		endMsg := robotgo.ShowAlert("提示", "录制完毕, 点击确定开始播放，开始后单击右键退出", "确定", "取消")
 		if endMsg == 0 { //确定0，取消1
-
 			quit := false
 			go WriteMouseLeftClickList(&quit, &mousePointList)
 			quit = robotgo.AddEvent("mright")
 		}
 	}
-	fmt.Println("感谢使用")
 }
